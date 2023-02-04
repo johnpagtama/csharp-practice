216c8966758ba8679e882f96496b6f099dd20856
johnpagtama <pagtamajr@gmail.com>
2023-02-04 01:05:31 -0800

    Added Solution and Project
    Added project for demonstrating built in value types.

---
+++ BuiltInValueTypes/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+namespace BuiltInValueTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool  bool_true = true;
+            Console.WriteLine("Boolean value 1 " + bool_true);
+
+
+
+            bool  bool_false = false;
+            Console.WriteLine("Boolean value 2 " + bool_false);
+
+
+
+            byte  byte_min = byte.MinValue;
+            Console.WriteLine("Minimum byte value " + byte_min);
+
+
+
+            byte  byte_max = byte.MaxValue;
+            Console.WriteLine("Maximum byte value " + byte_max);
+
+
+
+            sbyte sbyte_min = sbyte.MinValue;
+            Console.WriteLine("Minimum sbyte value " + sbyte_min);
+
+
+
+            sbyte sbyte_max = sbyte.MaxValue;
+            Console.WriteLine("Maximum sbyte value " + sbyte_max);
+
+
+
+            short short_min = short.MinValue;
+            Console.WriteLine("Minimum short value " + short_min);
+
+
+
+            short short_max = short.MaxValue;
+            Console.WriteLine("Maximum short value " + short_max);
+
+
+
+            ushort ushort_min = ushort.MinValue;
+            Console.WriteLine("Minimum ushort value " + ushort_min);
+
+
+
+            ushort ushort_max = ushort.MaxValue;
+            Console.WriteLine("Maximum ushort value " + ushort_max);
+
+
+
+            int int_min = int.MinValue;
+            Console.WriteLine("Minimum int value " + int_min);
+
+
+
+            int int_max = int.MaxValue;
+            Console.WriteLine("Maximum int value " + int_max);
+
+
+
+            uint uint_min = uint.MinValue;
+            Console.WriteLine("Minimum uint value " + uint_min);
+
+
+
+            uint uint_max = uint.MaxValue;
+            Console.WriteLine("Maximum uint value " + uint_max);
+
+
+
+            long long_min = long.MinValue;
+            Console.WriteLine("Minimum long value " + long_min);
+
+
+
+            long long_max = long.MaxValue;
+            Console.WriteLine("Maximum long value " + long_max);
+
+
+
+            ulong ulong_min = ulong.MinValue;
+            Console.WriteLine("Minimum ulong value " + ulong_min);
+
+
+
+            ulong ulong_max = ulong.MaxValue;
+            Console.WriteLine("Maximum ulong value " + ulong_max);
+
+
+
+            nint nint_min = nint.MinValue;
+            Console.WriteLine("Minimum nint value " + nint_min);
+
+
+
+            nint nint_max = nint.MaxValue;
+            Console.WriteLine("Maximum nint value " + nint_max);
+
+
+
+            nuint nuint_min = nuint.MinValue;
+            Console.WriteLine("Minimum nuint value " + nuint_min);
+
+
+
+            nuint nuint_max = nuint.MaxValue;
+            Console.WriteLine("Maximum nuint value " + nuint_max);
+
+
+
+            char char_min = char.MinValue;
+            Console.WriteLine("Minimum char value " + char_min);
+
+
+
+            char char_max = char.MaxValue;
+            Console.WriteLine("Maximum char value " + char_max);
+
+
+
+            decimal decimal_min = decimal.MinValue;
+            Console.WriteLine("Minimum decimal value " + decimal_min);
+
+
+
+            decimal decimal_max = decimal.MaxValue;
+            Console.WriteLine("Maximum decimal value " + decimal_max);
+
+
+
+            double double_min = double.MinValue;
+            Console.WriteLine("Minimum double value " + double_min);
+
+
+
+            double double_max = double.MaxValue;
+            Console.WriteLine("Maximum double value " + double_max);
+
+
+
+            float float_min = float.MinValue;
+            Console.WriteLine("Minimum float value " + float_min);
+
+
+
+            float float_max = float.MaxValue;
+            Console.WriteLine("Maximum float value " + float_max);
+        }
+    }
+}
